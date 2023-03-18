9b0312a41bedc314ebde8d9f6b20886ec3f4cb05
imrandev20 <imrandev20@gmail.com>
2023-03-18 22:43:07 +0600

    Removed unnecessary div from deliver-address-modal

--- src/components/checkout/delivery-address-modal.tsx
+++ src/components/checkout/delivery-address-modal.tsx
@@ -16,23 +16,21 @@ const style = {
 const DeliveryAddressModal = ({ open, setOpen }) => {
   const handleClose = () => setOpen(false);
   return (
-    <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-    </div>
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Text in a modal
+        </Typography>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+        </Typography>
+      </Box>
+    </Modal>
   );
 };
 
