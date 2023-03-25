1b3a24966d57e8dad159cef703445379165148ce
imrandev20 <imrandev20@gmail.com>
2023-03-26 04:48:19 +0600

    forgot

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
 import type { NextPage } from "next";
 import Hero from "../components/home/hero";
+import { Box, Grid } from "@mui/material";
 
 const Home: NextPage = () => {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 };
 
 export default Home;
